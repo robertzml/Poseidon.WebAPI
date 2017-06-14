27bb256860027e37069c90d3fac1537122e7e003
robertzml <robertzml@126.com>
2017-06-14 10:35:49 +0800

    Add Poseidon.WebAPI.Core project.
    Signed-off-by: robertzml <robertzml@126.com>

---
+++ Poseidon.WebAPI.Core/DL/Attachment.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poseidon.WebAPI.Core.DL
+{
+    using Poseidon.Base.Framework;
+
+    /// <summary>
+    /// 附件类
+    /// </summary>
+    public class Attachment : BaseEntity
+    {
+        #region Property
+        /// <summary>
+        /// 名称
+        /// </summary>
+        [Display(Name = "名称")]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 文件类型
+        /// </summary>
+        [Display(Name = "文件类型")]
+        public string FileType { get; set; }
+
+        /// <summary>
+        /// 路径
+        /// </summary>
+        [Display(Name = "路径")]
+        public string Path { get; set; }
+        #endregion //Property
+    }
+}
